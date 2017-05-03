556e21ab758267a9f275f5b4a101e14ccdc6b0e1
Ben Kelly <benkellysoftware@gmail.com>
2017-05-03 16:51:45 +1000

    Working with default AES setup now (hopefully)

--- lib/comms.py
+++ lib/comms.py
@@ -34,7 +34,7 @@ class StealthConn(object):
 
     def send(self, data):
         if self.cipher:
-            encrypted_data = self.encrypt(Padding.appendPadding(plaintext,blocksize=Padding.AES_blocksize,mode='CTR'))
+            encrypted_data = self.cipher.encrypt(self.pad(data))
             if self.verbose:
                 print("Original data: {}".format(data))
                 print("Encrypted data: {}".format(repr(encrypted_data)))
@@ -55,7 +55,7 @@ class StealthConn(object):
 
         encrypted_data = self.conn.recv(pkt_len)
         if self.cipher:
-            data = self.cipher.decrypt(encrypted_data)
+            data = self.unpad(self.cipher.decrypt(encrypted_data))
             if self.verbose:
                 print("Receiving packet of length {}".format(pkt_len))
                 print("Encrypted data: {}".format(repr(encrypted_data)))
@@ -65,34 +65,26 @@ class StealthConn(object):
 
         return data
 
-<<<<<<< HEAD
-    def encrypt(self, plaintext):
-        return self.cipher.encrypt(plaintext)
+    def pad(self, m, pad_length=16):
+        # Work out how many bytes need to be added
+        required_padding = pad_length - (len(m) % pad_length)
+        # Use a bytearray so we can add to the end of m
+        b = bytearray(m)
+        # Then k-1 zero bytes, where k is the required padding
+        b.extend(bytes("\x00" * (required_padding-1), "ascii"))
+        # And finally adding the number of padding bytes added
+        b.append(required_padding)
+        return bytes(b)
 
-    def decrypt(self, ciphertext):
-        return self.cipher.decrypt(ciphertext)
-=======
-def pad(m, pad_length):
-    # Work out how many bytes need to be added
-    required_padding = pad_length - (len(m) % pad_length)
-    # Use a bytearray so we can add to the end of m
-    b = bytearray(m)
-    # Then k-1 zero bytes, where k is the required padding
-    b.extend(bytes("\x00" * (required_padding-1), "ascii"))
-    # And finally adding the number of padding bytes added
-    b.append(required_padding)
-    return bytes(b)
-
-def unpad(m, pad_length):
-    # The last byte should represent the number of padding bytes added
-    required_padding = m[-1]
-    # Ensure that there are required_padding - 1 zero bytes
-    if m.count(bytes([0]), -required_padding, -1) == required_padding - 1:
-        return m[:-required_padding]
-    else:
-        # Raise an exception in the case of an invalid padding
-        raise AssertionError("Padding was invalid")
->>>>>>> c7d9c40dd0bbbb460624650743f117bfaebcbe77
+    def unpad(self, m, pad_length=16):
+        # The last byte should represent the number of padding bytes added
+        required_padding = m[-1]
+        # Ensure that there are required_padding - 1 zero bytes
+        if m.count(bytes([0]), -required_padding, -1) == required_padding - 1:
+            return m[:-required_padding]
+        else:
+            # Raise an exception in the case of an invalid padding
+            raise AssertionError("Padding was invalid")
 
     def close(self):
         self.conn.close()
